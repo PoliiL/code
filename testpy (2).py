ea298759c047f575d747d39a0b03791ba0a52cc4
PoliiL <slowedmusic627@gmail.com>
2025-06-23 00:07:34 +0400

    Update testpy (2).py

--- testpy (2).py
+++ testpy (2).py
@@ -1,9 +1,14 @@
-# A function that checks if a year is a leap year
-def is_leap(year):
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-# Example
-year = 2025
-if is_leap(year):
-    print(f"The year {year} is a leap year")
-else:
-    print(f"The year {year} is not a leap year")
+def is_prime(number):
+    if number > 1:
+        for i in range(2, int(number / 2) + 1):
+            if number % i == 0:
+                print(f"{number} is not a prime number.")
+                break
+        else:
+            print(f"{number} is a prime number.")
+    else:
+        print(f"{number} is not a prime number.")
+        
+is_prime(19)
+is_prime(7)
+is_prime(14)
